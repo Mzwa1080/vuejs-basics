f7d9185c9de7ae8e31b70ca404a4a2179a02e798
Mzwa1080 <mshawnlouw@gmail.com>
2023-12-12 11:28:07 +0200

    first product section

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="wrapper">
+      <div class="row" v-if="products.length">
+        <div class="prodCard" v-for="product in products" :key="product.id">
+          <div class="card-header">
+            <h3>{{ product.prodName }}</h3>
+          </div>
+          <div class="card-body">
+            <!-- Attribute binding - - is assigning a product value to an attribute -->
+            <img :src="product.image" :alt="product.prodName" loading="lazy"  @mouseenter="onMouseEnter"
+              @mouseleave="onMouseLeave">
+            <p :class="product.amount > 420 ? 'expensive' : 'cheap'">R{{ product.amount.toFixed(2) }}</p>
+            <p :class="product.id % 2 ==0 ? 'red' : 'blue'">R{{ product.amount.toFixed(2) }}</p>
+
+          </div>
+          <div class="card-footer">
+            <a href="#" type="button">View Details</a>
+          </div>
+        </div>
+
+      </div>
+      <div v-else>
+       <h2>Yoo</h2>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  // name: 'ProductsComp',
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          prodName: 'Python Crash Course',
+          category: 'Python',
+          amount: 400.9999,
+          image: 'https://i.postimg.cc/MKDLBPYm/Python-Crash-Course-cover.jpg'
+        },
+        {
+          id: 2,
+          prodName: 'Python Programming',
+          category: 'Python',
+          amount: 900.4586,
+          image: 'https://i.postimg.cc/rFcbKX5p/Python-Programming-for-Beginners-cover.jpg'
+        },
+        {
+          id: 3,
+          prodName: 'Mastering C++',
+          category: 'C++',
+          amount: 1800.896,
+          image: 'https://i.postimg.cc/Ss9zrpfT/Mastering-C-Programming.jpg'
+        },
+      ]
+    }
+  },
+  computed() { },
+  methods: {
+    onMouseEnter() {
+      alert('Hello there')
+    },
+    onMouseLeave() {
+      alert('Bye')
+    },
+    // Can I add the color on the mouse leave
   }
+
 }
 </script>
 
@@ -23,4 +83,34 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+
+.wrapper :is(.row) {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.prodCard {
+  width: 18rem;
+}
+
+.prodCard :is(>*, img) {
+  width: 100%;
+  aspect-ratio: 1/1;
+}
+
+.expensive {
+  font-weight: bolder;
+  color: darkkhaki;
+}
+.red{
+  background-color: red;
+}
+.blue{
+  background-color: blue;
+  box-shadow: 1vw .1vw .1vw .1vw ;
+}
+
+.cheap {
+  color: black;
+}</style>
